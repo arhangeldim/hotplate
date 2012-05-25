2f31ba152125d5202474f976855e96fc64ebea5a
arhangeldim <arhangeldim@815ace75-6b3a-750c-854f-b4178ac2cb4a>
2012-05-25 14:01:37 +0000

    MINOR Added util class. It has function to format time
    git-svn-id: https://hotplate.googlecode.com/svn/trunk@52 815ace75-6b3a-750c-854f-b4178ac2cb4a

---
+++ src/gmc/hotplate/util/Utils.java
@@ -0,0 +1,55 @@
+package gmc.hotplate.util;
+
+public final class Utils {
+
+    private Utils() {
+        throw new AssertionError("Not for instantiations!");
+    }
+
+    /*
+     * @param time - time in seconds
+     * Returns time in format hh:mm:ss
+     * If hours == 0                    mm:ss
+     * If hours == 0 && minutes < 10    0m:ss
+     * If hours < 10                    h:mm:ss
+     *
+     */
+    public static String format(int time) {
+        if (time <= 0) {
+            return "00:00";
+        }
+        int seconds = 0;
+        int minutes = 0;
+        int hours = 0;
+        hours = time / 3600;
+        time -= hours * 3600;
+        minutes = time / 60;
+        time -= minutes * 60;
+        seconds = time;
+        return format(hours, minutes, seconds);
+    }
+
+    private static String format(int hours, int minutes, int seconds) {
+        // Overflow
+        if (hours >=24 || minutes >=60 || seconds >= 60) {
+            return "00:00";
+        }
+        StringBuilder builder = new StringBuilder();
+
+        if (hours != 0) {
+            builder.append(String.valueOf(hours));
+            builder.append(":");
+        }
+        if ((minutes < 10 && hours != 0) || (minutes == 0)) {
+            builder.append(0);
+        }
+        builder.append(String.valueOf(minutes));
+        builder.append(":");
+        if (seconds < 10) {
+            builder.append(0);
+        }
+        builder.append(String.valueOf(seconds));
+        return builder.toString();
+    }
+
+}
